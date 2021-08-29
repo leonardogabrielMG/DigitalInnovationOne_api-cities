42ee0f2cbba2925f9b2de50359521f6cc04c2127
Leonardo Oliveira <leonardogbrl85@gmail.com>
2021-08-28 23:25:58 -0300

    DIO - QA everis 2 - api-cities

---
+++ src/main/java/com/github/leonardogabrielMG/distCidades/countries/CountryRepository.java
@@ -0,0 +1,7 @@
+package com.github.leonardogabrielMG.distCidades.countries;
+
+import com.github.leonardogabrielMG.distCidades.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
---
+++ src/test/java/com/github/leonardogabrielMG/distCidades/DistCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leonardogabrielMG.distCidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
